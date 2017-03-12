7d60d8da2385aad176e43f7c5fdc2cf81d5be9da
Omer Shemsheyan <omershemesh95@gmail.com>
2017-03-12 15:38:38 +0200

    added setup statistics (initial)

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_restful import Api, Resource
 from flask_pymongo import PyMongo
+from webargs.flaskparser import use_args, use_kwargs, parser, abort
+from marshmallow import fields
 
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'dallas'
@@ -9,10 +11,48 @@ api = Api(app)
 mongo = PyMongo(app)
 
 
-class ClustersResource(Resource):
+class SetupResource(Resource):
+
     def get(self):
-        clusters = mongo.db.cluster.find()
+        setups_count = mongo.db.setup.find().count()
         output = []
+        pipe = [{'$group': {'_id': None, 'average_dcs_count': {'$avg': '$dcs_count'}}}]
+        avg_dcs = list(mongo.db.setup.aggregate(pipeline=pipe))
+
+        stats = {'setups_count': setups_count, 'average_dcs_count': avg_dcs[0]['average_dcs_count']}
+
+        return stats
+
+
+class DataCenterResource(Resource):
+    def get(self):
+        datacenters = mongo.db.datacenter.find()
+        output = []
+
+        for datacenter in datacenters:
+            output.append(datacenter)
+        return output
+
+
+class ClustersResource(Resource):
+    args = {
+        'vms': fields.Integer(required=False),
+        'hosts': fields.Integer(required=False)
+    }
+
+    @use_kwargs(args)
+    def get(self, vms, hosts):
+        output = []
+
+        if vms and not hosts:
+            clusters = mongo.db.cluster.find({"vms_count": {"$gte": vms}})
+        elif hosts and not vms:
+            clusters = mongo.db.cluster.find({"hosts_count": {"$gte": hosts}})
+        elif hosts and vms:
+            clusters = mongo.db.cluster.find(
+                {"hosts_count": {"$gte": hosts}, "vms_count": {"$gte": vms}})
+        else:
+            clusters = mongo.db.cluster.find()
 
         for cluster in clusters:
             output.append(cluster)
@@ -22,8 +62,8 @@ class ClustersResource(Resource):
 
 class HostsResource(Resource):
     def get(self):
-        hosts = mongo.db.host.find()
         output = []
+        hosts = mongo.db.host.find()
 
         for host in hosts:
             output.append(host)
@@ -65,11 +105,16 @@ class TemplatesResource(Resource):
             output.append(template)
         return output
 
+api.add_resource(SetupResource, '/setups/statistics')
 
 # hosts endpoints
+api.add_resource(DataCenterResource, '/datacenters')
 api.add_resource(HostResource, '/hosts/<host_id>')
+
 api.add_resource(HostsStatisticsResource, '/hosts/statistics')
 api.add_resource(HostsResource, '/hosts')
+
+
 
 # clusters endpoints
 api.add_resource(ClustersResource, '/clusters')
